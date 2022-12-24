7e92b8d2414fc7f90bb5fe2dc2e05a836d7069eb
jrwerkman <jrwerkman@gmail.com>
2022-12-24 12:08:51 +0100

    day 11 part 1

---
+++ src/main/java/nl/jrwer/challenge/advent/input/MultipleLinesInputLoader.java
@@ -0,0 +1,54 @@
+package nl.jrwer.challenge.advent.input;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import nl.jrwer.challenge.advent.Day04;
+
+public abstract class MultipleLinesInputLoader<T> {
+	
+	private final String file;
+	private final int amountLines;
+	private List<T> list = new ArrayList<>();
+	
+	public MultipleLinesInputLoader(String file, int amountLines) {
+		this.file = file;
+		this.amountLines = amountLines;
+	}
+	
+	public List<T> getInput() {
+		
+        try (InputStream inputStream = Day04.class.getClassLoader().getResourceAsStream(file);
+        		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+        	int i = 0;
+        	String[] lines = new String[this.amountLines];
+        	String line;
+        	
+            while ((line = reader.readLine()) != null) {
+            	if(line.isBlank())
+            		continue;
+            	
+            	lines[i] = line;
+            	i++;
+            	
+            	if(i == amountLines) {
+            		i=0;
+	            	T obj = handleLines(lines);
+	            	
+	            	if(obj != null)
+	            		list.add(obj);
+            	}
+            }
+        } catch (IOException e) {
+			e.printStackTrace();
+		}
+        
+        return list;
+	}
+	
+	protected abstract T handleLines(String[] lines);
+}
